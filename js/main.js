7c73a89e1466da65271b14d12c06bf74e9e795cc
flyingisfreedom5 <berkay_tor@hotmail.com>
2022-04-07 18:56:17 +0300

    JS changes commit

--- js/main.js
+++ js/main.js
@@ -1,119 +1,209 @@
 /*----- constants -----*/
+const suits = ['s', 'c', 'd', 'h'];
+const ranks = ['02', '03', '04', '05', '06', '07', '08', '09', '10', 'J', 'Q', 'K', 'A'];
+const MSG_LOOKUP = {
+  null: 'Good Luck!',
+  'T': "It's a Tie",
+  'P': 'Player Wins!',
+  'D': 'Dealer Wins',
+  'PBJ': 'Player Has Blackjack ',
+  'DBJ': 'Dealer Has Blackjack ',
+};
+const mainDeck = buildMainDeck();
 
-const masterDeck = [
-    {name: 'c02', value: 2},
-    {name: 'c03', value: 3},
-    {name: 'c04', value: 4},
-    {name: 'c05', value: 5},
-    {name: 'c06', value: 6},
-    {name: 'c07', value: 7},
-    {name: 'c08', value: 8},
-    {name: 'c09', value: 9},
-    {name: 'c10', value: 10},
-    {name: 'cA', value: 11},
-    {name: 'cJ', value: 10},
-    {name: 'cQ', value: 10},
-    {name: 'cK', value: 10},
-    {name: 'd02', value: 2},
-    {name: 'd03', value: 3},
-    {name: 'd04', value: 4},
-    {name: 'd05', value: 5},
-    {name: 'd06', value: 6},
-    {name: 'd07', value: 7},
-    {name: 'd08', value: 8},
-    {name: 'd09', value: 9},
-    {name: 'd10', value: 10},
-    {name: 'dA', value: 11},
-    {name: 'dJ', value: 10},
-    {name: 'dQ', value: 10},
-    {name: 'dK', value: 10},
-    {name: 'h02', value: 2},
-    {name: 'h03', value: 3},
-    {name: 'h04', value: 4},
-    {name: 'h05', value: 5},
-    {name: 'h06', value: 6},
-    {name: 'h07', value: 7},
-    {name: 'h08', value: 8},
-    {name: 'h09', value: 9},
-    {name: 'h10', value: 10},
-    {name: 'hA', value: 11},
-    {name: 'hJ', value: 10},
-    {name: 'hQ', value: 10},
-    {name: 'hK', value: 10},
-    {name: 's02', value: 2},
-    {name: 's03', value: 3},
-    {name: 's04', value: 4},
-    {name: 's05', value: 5},
-    {name: 's06', value: 6},
-    {name: 's07', value: 7},
-    {name: 's08', value: 8},
-    {name: 's09', value: 9},
-    {name: 's10', value: 10},
-    {name: 'sA', value: 11},
-    {name: 'sJ', value: 10},
-    {name: 'sQ', value: 10},
-    {name: 'sK', value: 10},
-];
-console.log(masterDeck);
+/*----- app's state (variables) -----*/
+let deck;  // shuffled deck
+let pHand, dHand;  // player/dealer hands (arrays)
+let pTotal, dTotal;  // best point value of hand
+let cash, bet;  // cash how much money we have & bet is the amount of the bet
+let outcome;  // result of the hand (see MSG_LOOKUP)
 
-// /*----- app's state (variables) -----*/
-let playerHand;
-let dealerHand;
-let winner; 
-let aceCount; 
-let ShuffledDeck;
 /*----- cached element references -----*/
-
-const pResultEl = document.querySelector('.p-total-p');
-const dResultEl = document.querySelector('.d-total-d');
-const betAcceptEl = document.querySelector(".bet-accept-btn")
-const hitEl = document.querySelector('.hit-btn');
-const standEl = document.querySelector('.stand-btn');
-const casinoChipsEl = document.querySelector('.casino-chips');
-const betAmountEl = document.querySelector('.bet-amount');
-const cashEl = document.querySelector('.cash-amount');
-
-
-
-
-
-
-
+const msgEl = document.getElementById('msg');
+const dealerHandEl = document.querySelector('.dealer-area');
+const dealerTotalEl = document.querySelector('.d-total-d');
+const playerHandEl = document.querySelector('.player-area');
+const playerTotalEl = document.querySelector('.p-total-p');
+const betEl = document.getElementById('bet-amount');
+const cashEl = document.getElementById('cash-amount');
+const hitStandBtnsEl = document.querySelector('.buttons');
+const betControlsEl = document.querySelector('.casino-chips');
+const betAcceptBtnEl = document.getElementById('bet-accept-btn');
+const betBtns = document.querySelectorAll('.casino-chips > button');
 
 /*----- event listeners -----*/
-// document.querySelector('body')
-//     .addEventListener('click', handleChoice);
+betAcceptBtnEl.addEventListener('click', handleAccept);
+document.querySelector('.hit-btn').addEventListener('click', handleHit);
+document.querySelector('.stand-btn').addEventListener('click', handleStand);
+document.querySelector('.casino-chips').addEventListener('click', handleBet);
+
 /*----- functions -----*/
 init();
 
-
-function init() {
-    playerHand = [];
-    dealerHand = [];
-    winner = '';
-    ShuffledDeck = getNewShuffledDeck();   
+function handleStand() {
+  dealerPlay(function() {
+    if (pTotal === dTotal) {
+      outcome = 'T';
+    } else if (dTotal > pTotal && dTotal <= 21 ) {
+      outcome = 'D';
+    } else {
+      outcome = 'P';
+    }
+    settleBet();
     render();
+  });
 }
 
+function dealerPlay(cb) {
+  outcome = 'D';
+  renderHands();
+  // while (dTotal < 17) {
+    setTimeout(function() {
+      if (dTotal < 17  && dTotal < pTotal) {
+        dHand.push(deck.pop());
+        dTotal = getHandTotal(dHand);
+        dealerPlay(cb);
+      } else {
+        cb();
+      }
+    }, 1000);
+  // }
+}
 
+function handleHit() {
+  pHand.push(deck.pop());
+  pTotal = getHandTotal(pHand);
+  if (pTotal > 21) {
+    outcome = 'D';
+    settleBet();
+  }
+  render();
+}
+
+function handleAccept(evt) {
+  const btn = evt.target;
+  if (btn.tagName !== 'BUTTON') return;
+  const betAmt = parseInt(btn.innerText.replace('$', ''));
+  bet += betAmt;
+  cash -= betAmt;
+  render();
+}
+
+function handleBet() {
+  outcome = null;
+  deck = getNewShuffledDeck();
+  dHand = [];
+  pHand = [];
+  dHand.push(deck.pop(), deck.pop());
+  pHand.push(deck.pop(), deck.pop());
+  // Check for blackjack
+  dTotal = getHandTotal(dHand);
+  pTotal = getHandTotal(pHand);
+  if (dTotal === 21 && pTotal === 21) {
+    outcome = 'T';
+  } else if (dTotal === 21) {
+    outcome = 'DBJ';
+  } else if (pTotal === 21) {
+    outcome = 'PBJ';
+  }
+  if (outcome) settleBet();
+  render();
+}
+
+function settleBet() {
+  if (outcome === 'PBJ') {
+    cash += bet + (bet * 1.5);
+  } else if (outcome === 'P') {
+    cash += bet * 2;
+  }
+  bet = 0;
+}
+
+// compute the best score for the hand passed in
+function getHandTotal(hand) {
+  let total = 0;
+  let aces = 0;
+  hand.forEach(function(card) {
+    total += card.value;
+    if (card.value === 11) aces++;
+  });
+  while (total > 21 && aces) {
+    total -= 10;
+    aces--;
+  }
+  return total;
+}
+
+// initialize state, then call render()
+function init() {
+  outcome = null;
+  dHand = [];
+  pHand = [];
+  pTotal = dTotal = 0;
+  cash = 1000;
+  bet = 0;
+  render();
+}
+
+// Visualize all state to the DOM
 function render() {
-    renderHand();
+  renderHands();
+  cashEl.innerHTML = cash;
+  betEl.innerHTML = bet;
+  renderControls();
+  renderBetBtns();
+  msgEl.innerHTML = MSG_LOOKUP[outcome];
+}
+
+function renderBetBtns() {
+  betBtns.forEach(function(btn) {
+    const btnAmt = parseInt(btn.innerText.replace('$', ''));
+    btn.disabled = btnAmt > cash;
+  });
+}
+
+function renderControls() {
+  betControlsEl.style.visibility = handInPlay() ? 'hidden' : 'visible';
+  hitStandBtnsEl.style.visibility = handInPlay() ? 'visible' : 'hidden'; 
+  betAcceptBtnEl.style.visibility = handInPlay() ? 'hidden' : 'visible';
+}
+
+function renderHands() {
+  playerTotalEl.innerHTML = pTotal;
+  dealerTotalEl.innerHTML = outcome ? dTotal : '??';
+  playerHandEl.innerHTML = pHand.map(card => `<div class="card ${card.face}"></div>`).join('');
+  dealerHandEl.innerHTML = dHand.map((card, idx) => `<div class="card ${idx === 1 && !outcome ? 'back' : card.face}"></div>`).join('');
+}
+
+function handInPlay() {
+  return pHand.length && !outcome;
 }
 
 function getNewShuffledDeck() {
-   
-    const tempDeck = [...masterDeck];
-    const newShuffledDeck = [];
-    while (tempDeck.length) {
-      // Get a random index for a card still in the tempDeck
-      const rndIdx = Math.floor(Math.random() * tempDeck.length);
-      // Note the [0] after splice - this is because splice always returns an array and we just want the card object in that array
-      newShuffledDeck.push(tempDeck.splice(rndIdx, 1)[0]);
-    }
-    return newShuffledDeck;
+  // Create a copy of the mainDeck (leave mainDeck untouched!)
+  const tempDeck = [...mainDeck];
+  const newShuffledDeck = [];
+  while (tempDeck.length) {
+    // Get a random index for a card still in the tempDeck
+    const rndIdx = Math.floor(Math.random() * tempDeck.length);
+    // Note the [0] after splice - this is because splice always returns an array and we just want the card object in that array
+    newShuffledDeck.push(tempDeck.splice(rndIdx, 1)[0]);
+  }
+  return newShuffledDeck;
 }
 
-
-
-
-
+function buildMainDeck() {
+  const deck = [];
+  // Use nested forEach to generate card objects
+  suits.forEach(function(suit) {
+    ranks.forEach(function(rank) {
+      deck.push({
+        // The 'face' property maps to the library's CSS classes for cards
+        face: `${suit}${rank}`,
+        // Setting the 'value' property for game of blackjack, not war
+        value: Number(rank) || (rank === 'A' ? 11 : 10)
+      });
+    });
+  });
+  return deck;
+}
